86d2ed7b45517740d45463d213c56e6c57daf748
Venkatesh Dayananda <Venkatesh.Dayananda@genesys.com>
2016-01-19 20:03:36 +0530

    Initial Commit

--- server.py
+++ server.py
@@ -9,23 +9,25 @@ import image
 
 app = Flask(__name__)
 
-VLC_PATH = "vlc D:\\Designer\\vid\\test.avi --video-filter=scene --vout=dummy --start-time=3 --stop-time=15 --scene-ratio=10 --scene-path=D:\\Designer\\vid vlc://quit"
-
+inp = 'D:\\Designer\\vid\\test.avi'
 
 url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
 img = open('test.png','rb').read()
 
+def setPath(input_path,output_path = 'D:\\Designer\\vid'):
+	return "vlc "+input_path+" --video-filter=scene --vout=dummy --start-time=3 --stop-time=15 --scene-ratio=10 --scene-path="+output_path+" vlc://quit" 
 
 headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': 'edd62e5fd2a0449b890a59bf6cdfb24e'}
-
-def hello():
-    resp =  os.popen(VLC_PATH)
+@app.route('/getFeature/<sting:img_path>')   
+def hello(img_path):
+   
+    resp =  os.popen(setPath(img_path))
     return "Done"
 
-@app.route('/')    
+#@app.route('/')    
 def mic_api():
 	#print img is None
-	r = requests.post(url, data=img, headers = headers)
+	r = requests.post(url, data=img,headers = headers)
 	return str(r.json())
 
 
