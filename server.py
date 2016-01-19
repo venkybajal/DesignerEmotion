8f4f96cf73fa7716a54156ae1d577070e2e3607f
Venkatesh Dayananda <Venkatesh.Dayananda@genesys.com>
2016-01-20 01:53:51 +0530

    Score Computation Added

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ import base64
 import json
 import image
 import glob
+import operator
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
 headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': 'edd62e5fd2a0449b890a59bf6cdfb24e'}
 
 result = {}
-
+emotion = {}
 #Get VLC Path
 def setPath(input_path,output_path = 'D:\\Designer'):
 	return "vlc "+input_path+" --video-filter=scene --vout=dummy --start-time=3 --stop-time=7 --scene-ratio=10 --scene-path="+output_path+" vlc://quit" 
@@ -38,22 +39,33 @@ def removeImageFiles():
 		os.remove(name)
 		i+=1
 
+def findEmotionWithMaxScore():
+	return max(emotion.iteritems(), key=operator.itemgetter(1))[0]
+
+
+def computeEmotionScore(array):
+	for i in array[0]['scores']:
+		if i in emotion:
+			emotion[i] += float(array[0]['scores'][i])
+		else:
+			emotion[i] = float(array[0]['scores'][i])
+
 @app.route('/')
 @app.route('/getFeature/<string:img_path>')       
 def mic_api(img_path = 'D:\\Designer\\vid\\test.mp4'):
 	#print img is None
-	removeImageFiles()
-	generateImages(img_path)
-	renameImageFiles()
+	#removeImageFiles()
+	#generateImages(img_path)
+	#renameImageFiles()
 	for name in glob.glob('*.png'):
 		getScores(name)
-	return str(result)
+	return jsonify(emotion = findEmotionWithMaxScore())
 
 def getScores(filename):
 	img = open(filename,'rb').read()
 	if img is not None:
 		r = requests.post(url, data=img,headers = headers)
-		result[filename] =  r.text
+		computeEmotionScore(r.json())
 	return "WError"
 
 
