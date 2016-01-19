5810041bdd537458acb7e5a4ce26bb101bd40908
Venkatesh Dayananda <Venkatesh.Dayananda@genesys.com>
2016-01-19 21:04:27 +0530

    Working model 1

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask,jsonify
 from subprocess import call
 import requests
 import base64
@@ -11,25 +11,31 @@ app = Flask(__name__)
 
 inp = 'D:\\Designer\\vid\\test.avi'
 
+#api
 url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
-img = open('test.png','rb').read()
-
-def setPath(input_path,output_path = 'D:\\Designer\\vid'):
-	return "vlc "+input_path+" --video-filter=scene --vout=dummy --start-time=3 --stop-time=15 --scene-ratio=10 --scene-path="+output_path+" vlc://quit" 
-
 headers = {'Content-Type': 'application/octet-stream','Ocp-Apim-Subscription-Key': 'edd62e5fd2a0449b890a59bf6cdfb24e'}
-@app.route('/getFeature/<sting:img_path>')   
-def hello(img_path):
-   
+
+
+
+#Get VLC Path
+def setPath(input_path,output_path = 'D:\\Designer'):
+	return "vlc "+input_path+" --video-filter=scene --vout=dummy --start-time=3 --stop-time=7 --scene-ratio=10 --scene-path="+output_path+" vlc://quit" 
+
+
+def generateImages(img_path): 
     resp =  os.popen(setPath(img_path))
-    return "Done"
+    return resp
 
-#@app.route('/')    
-def mic_api():
+@app.route('/')
+@app.route('/getFeature/<string:img_path>')       
+def mic_api(img_path = 'D:\\Designer\\vid\\test.avi' ):
 	#print img is None
-	r = requests.post(url, data=img,headers = headers)
-	return str(r.json())
-
+	generateImages(img_path)
+	img = open('test.png','rb').read()
+	if img is not None:
+		r = requests.post(url, data=img,headers = headers)
+		return r.text()
+	return "WError"
 
 if __name__ == '__main__':
 	app.debug = True
